8721b6002ffec19d44f1a69874ea56d2325e73ce
emines0 <emin.sefa1@gmail.com>
2024-03-02 18:02:56 +0100

    navigation

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,22 @@
-import '@/assets/styles/global.css'
+import "@/assets/styles/global.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
-  title: 'Property Pulse | Find The Perfect Rental',
-  description: 'Find the perfect rental property with Property Pulse. Search for apartments, houses, and condos for rent in your area.',
-  keywords: 'rental, find rentals, find properties'
-}
+  title: "Property Pulse | Find The Perfect Rental",
+  description:
+    "Find the perfect rental property with Property Pulse. Search for apartments, houses, and condos for rent in your area.",
+  keywords: "rental, find rentals, find properties",
+};
 
-const MainLayout = ({children}) => {
+const MainLayout = ({ children }) => {
   return (
-    <html lang='en'>
-        <body>   
-            <div>{children}</div>
-        </body>
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>{children}</main>
+      </body>
     </html>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
