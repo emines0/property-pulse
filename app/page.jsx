3f40530ca486bd232991d3d2021fa5f22a7d0593
emines0 <emin.sefa1@gmail.com>
2024-03-04 11:07:09 +0100

    properties page, recent properties on hompage

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import PropertiesPage from "./properties/page";
+import HomeProperties from "@/components/HomeProperties";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 };
